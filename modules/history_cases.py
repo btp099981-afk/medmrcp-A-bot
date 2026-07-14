8a9deeabfb887f4d3953b48021306fea768b2cda
btp099981-afk <btp099981@gmail.com>
2026-07-15 01:01:38 +0200

    Update history_cases.py

--- modules/history_cases.py
+++ modules/history_cases.py
@@ -1,1 +1,23 @@
+chest_pain_case = {
+    "title": "Chest Pain",
+    "patient": {
+        "name": "Ahmed",
+        "age": 55,
+        "gender": "Male",
+        "chief_complaint": "Chest pain"
+    },
+    "history": {
+        "onset": "2 hours ago",
+        "character": "Pressure-like pain",
+        "location": "Central chest",
+        "radiation": "Left arm",
+        "associated_symptoms": [
+            "Sweating",
+            "Shortness of breath"
+        ]
+    }
+}
 
+
+def get_case():
+    return chest_pain_case
