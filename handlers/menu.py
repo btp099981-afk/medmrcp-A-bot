ea060e8ccd8b4ab8d61eb0735cc444675dc13e14
btp099981-afk <btp099981@gmail.com>
2026-07-15 08:08:32 +0200

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,1 +1,64 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+# =========================
+# القائمة الرئيسية
+# =========================
+
+def main_menu():
+
+    keyboard = [
+
+        [
+            InlineKeyboardButton(
+                "❤️ Cardiovascular",
+                callback_data="cardio"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "🫁 Respiratory",
+                callback_data="respiratory"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "🧠 Neurology",
+                callback_data="neurology"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "🩺 Gastrointestinal",
+                callback_data="gi"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "🫘 Renal",
+                callback_data="renal"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "⭐ Premium Plan",
+                callback_data="premium"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "👤 My Account",
+                callback_data="account"
+            )
+        ]
+
+    ]
+
+
+    return InlineKeyboardMarkup(keyboard)
